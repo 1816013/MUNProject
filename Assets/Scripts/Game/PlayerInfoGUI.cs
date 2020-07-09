da41212f309a9df0cfdaf2eea56366a3ba24e6ea
1816013 <putajfgailwugquilg@iuauilg.com>
2020-07-09 09:17:33 +0900

    変更

---
+++ Assets/Scripts/Game/PlayerInfoGUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MonobitEngine;
+
+public class PlayerInfoGUI : MonobitEngine.MonoBehaviour
+{
+    public GameObject m_Canvas;
+    // プレイヤー情報を表示するためのテキスト
+    private Text m_PlayerInfoText;
+
+    // プレイヤー情報
+    private MonobitPlayer m_Player;
+
+    // プレイヤーのID
+    public int ID { get; private set; }
+
+    // 表示済みかどうか
+    public bool IsEntry { get; private set; }
+
+    private void Awake()
+    {
+        GameObject textObj = Instantiate(Resources.Load("uGUI_Text")) as GameObject;
+        textObj.transform.SetParent(this.transform);
+        //textObj.transform.localPosition;
+        m_PlayerInfoText = textObj.GetComponent<Text>();
+        m_PlayerInfoText.text = "NoEntry";
+        ID = int.MaxValue;
+        IsEntry = false;
+    }
+
+    public void Set(MonobitPlayer player)
+    {
+        m_Player = player;
+        ID = m_Player.ID;
+        IsEntry = true;
+        InfoUpdate();
+    }
+
+    public void Clear()
+    {
+        m_Player = null;
+        m_PlayerInfoText.text = "No Entry";
+        ID = int.MaxValue;
+        IsEntry = false;
+    }
+
+    public void InfoUpdate()
+    {
+        if(!IsEntry)
+        {
+            return;
+        }
+        string attribute = m_Player.isHost ? "Host" : "Guest";
+        m_PlayerInfoText.text = m_Player.ID + ":" + m_Player.name + " " + attribute;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Game/RoomWaitScene.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MonobitEngine;
+
+public class RoomWaitScene : MonobitEngine.MonoBehaviour
+{
+    private GameObject m_Camvas;
+    // ルーム参加数のオブジェクト
+    private GameObject m_RoomMemberCountObj;
+
+    // プレイヤー情報を表示するオブジェクト
+    private GameObject[] m_PlayerInfos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!MonobitNetwork.inRoom)
+        {
+            Debug.Log("ルームに入っていない");
+            return;
+        }
+        m_Camvas = Instantiate(Resources.Load("Canvas"))as GameObject;
+        m_RoomMemberCountObj = Instantiate(Resources.Load("uGUI_Text")) as GameObject;
+        m_RoomMemberCountObj.transform.SetParent(m_Camvas.transform);
+        m_RoomMemberCountObj.transform.localPosition = new Vector3(0, 0, 0);
+        UpdatePlayersCount();
+
+        // ルームに入れるプレイヤーの最大数分だけプレイヤー情報表示を作成する
+        m_PlayerInfos = new GameObject[MonobitNetwork.room.maxPlayers];
+        for(int i = 0; i < MonobitNetwork.room.maxPlayers; ++i)
+        {
+            // PlayerInfoGUI.csが追加されたPrefabをインスタンス化
+            m_PlayerInfos[i] = Instantiate(Resources.Load("PlayerInfoGUI")) as GameObject;
+            m_PlayerInfos[i].transform.SetParent(m_Camvas.transform);
+            m_PlayerInfos[i].transform.localPosition = new Vector3(10, 20 * i + 20, 0);
+        }
+
+        // 作成したプレイヤー情報表示に参加プレイヤーの情報を設定
+        int length = MonobitNetwork.playerList.Length;
+        for(int i = 0; i < length; ++i)
+        {
+            m_PlayerInfos[i].GetComponent<PlayerInfoGUI>().Set(MonobitNetwork.playerList[i]);
+            m_PlayerInfos[i].SetActive(true);
+        }
+    }
+
+    // 他プレイヤーが入室した際のコールバック
+    private void OnOtherPlayerConnected(MonobitEngine.MonobitPlayer newPlayer)
+    {
+        int length = m_PlayerInfos.Length;
+        for (int i = 0; i < length; ++i)
+        {
+            PlayerInfoGUI playerInfo = m_PlayerInfos[i].GetComponent<PlayerInfoGUI>();
+            if(playerInfo.IsEntry)
+            {
+                continue;
+            }
+            playerInfo.Set(newPlayer);
+            m_PlayerInfos[i].SetActive(true);
+            break;
+        }
+            UpdatePlayersCount();
+    }
+
+    // 他プレイヤーが退室した際のコールバック
+    private void OnOtherPlayerDisconnected(MonobitEngine.MonobitPlayer otherPlayer)
+    {
+        int length = m_PlayerInfos.Length;
+        for (int i = 0; i < length; ++i)
+        {
+            PlayerInfoGUI playerInfo = m_PlayerInfos[i].GetComponent<PlayerInfoGUI>();
+            if (playerInfo.ID != otherPlayer.ID)
+            {
+                continue;
+            }
+            playerInfo.Clear();
+            m_PlayerInfos[i].SetActive(false);
+            break;
+        }
+        UpdatePlayersCount();
+    }
+
+    // ルーム人数の表示を更新する
+    private void UpdatePlayersCount()
+    {
+        m_RoomMemberCountObj.GetComponent<Text>().text = MonobitNetwork.room.name +
+           "(" + MonobitNetwork.room.playerCount + "/" + MonobitNetwork.room.maxPlayers + ")";
+    }
+}
